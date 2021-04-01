45e148f92423a753eff907c0a8ab1202439186a1
hawk-garethf <67474273+hawk-garethf@users.noreply.github.com>
2021-04-01 20:40:33 +0100

    a

---
+++ js/lottieAnimationScript.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('lottieAnimation'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets9.lottiefiles.com/packages/lf20_nre2i7dc.json'
+})
