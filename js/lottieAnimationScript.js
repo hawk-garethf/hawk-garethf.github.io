741c7ca507e789a793af1cc01ae677eda68385ee
hawk-garethf <67474273+hawk-garethf@users.noreply.github.com>
2021-04-01 20:56:36 +0100

    s

--- js/lottieAnimationScript.js
+++ js/lottieAnimationScript.js
@@ -1,13 +1,13 @@
-
-                LottieInteractivity.create({
-                  mode:"scroll",
-                  player:'#firstLottie',
-                  actions: [
-                    {
-                      visibility: [0,1],
-                      type: "seek",
-                      frames: [0, 59]
-                    }
-                  ]  
-                });
-              
+                <script>
+                    LottieInteractivity.create({
+                    mode:"scroll",
+                    player:'#firstLottie',
+                    actions: [
+                        {
+                        visibility: [0,1],
+                        type: "seek",
+                        frames: [0, 59]
+                        }
+                    ]  
+                    });
+                </script>
